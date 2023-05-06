71d5c6d9dc3453ed6f9ddfefb243c856b2bf1b6a
magisystem0408 <61937077+magisystem0408@users.noreply.github.com>
2023-05-06 10:22:38 +0900

    inital commit

---
+++ config/routes.rb
@@ -0,0 +1,8 @@
+Rails.application.routes.draw do
+  get 'statics/index'
+  get 'static/index'
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Defines the root path route ("/")
+  # root "articles#index"
+end
